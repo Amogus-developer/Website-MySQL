1275668a1acbd73154e548906bb975233029606e
Vladimir <117392112+Amogus-developer@users.noreply.github.com>
2023-05-31 20:13:13 +0400

    MySQL update

--- src/main/kotlin/com/example/servingwebcontent/controllers/ServerController.kt
+++ src/main/kotlin/com/example/servingwebcontent/controllers/ServerController.kt
@@ -6,16 +6,35 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class ServerController() {
     @Autowired
     private lateinit var dataRepository: DataRepository
 
-    @GetMapping("/server-main")
+    @GetMapping("/servers")
     fun servers(model: Model): String {
         val dates: Iterable<Data> = dataRepository.findAll()
         model.addAttribute("dates", dates)
-        return "server-main"
+        return "servers"
+    }
+    @GetMapping("/add-server")
+    fun addServer(model: Model): String {
+        return "add-server"
+    }
+
+    @PostMapping("/add-server")
+    fun postServer(@RequestParam title: String,
+                   @RequestParam description: String,
+                   @RequestParam address: String,
+                   @RequestParam password: String,
+                   model: Model): String {
+        if (password == "я админ") {
+            val data = Data(title, description, address)
+            dataRepository.save(data)
+        }
+        return "redirect:/servers"
     }
 }
--- src/main/kotlin/com/example/servingwebcontent/models/Data.kt
+++ src/main/kotlin/com/example/servingwebcontent/models/Data.kt
@@ -6,18 +6,21 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
 @Entity
-class Data {
+class Data(private var title: String = "",
+           private var description: String = "",
+           private var address: String = "") {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private var id: Long = 0
-    private var title: String = ""
-    private var description: String = ""
 
     fun getId(): Long {return id}
     fun setId(id: Long) {this.id = id}
 
     fun getTitle(): String {return title}
     fun setTitle(title: String) {this.title = title}
+
+    fun getAddress(): String {return address}
+    fun setAddress(address: String) {this.address = address}
 
     fun getDescription(): String {return description}
     fun setDescription(description: String) {this.description = description}
