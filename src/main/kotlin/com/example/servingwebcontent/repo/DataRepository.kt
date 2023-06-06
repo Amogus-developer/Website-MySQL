dd193e42421aed88d3df8a5c33f86a680be994be
Vladimir <117392112+Amogus-developer@users.noreply.github.com>
2023-06-06 15:38:12 +0300

    Delete src directory

--- src/main/kotlin/com/example/servingwebcontent/repo/DataRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.servingwebcontent.repo
-
-import com.example.servingwebcontent.models.Data
-import org.springframework.data.repository.CrudRepository
-
-interface DataRepository: CrudRepository<Data, Long> {
-
-}
